aaaf459b3eef73f69ee42102bd4db1464419765c
Nicolas Nascimento <nico.nascimento.97@gmail.com>
2023-10-26 09:23:31 -0300

    Atividade Estruturas Condicionais

--- praticas/introducao-a-programacao/estruturas-condicionais/atividade_condicionais.js
+++ praticas/introducao-a-programacao/estruturas-condicionais/atividade_condicionais.js
@@ -1,63 +1,63 @@
 // // 1. Escreva um algoritmo para ler uma temperatura em graus Celsius,
 // // calcular e escrever o valor correspondente em graus Fahrenheit.
-// document.write("<br/> Exercicio 1 <br/>")
+document.write("<br/> <br/> <strong>Exercicio 1</strong> <br/>")
 
-// const celsius = 2;
-// const fahrenheit = (celsius * 9/5) + 32;
+const celsius = 2;
+const fahrenheit = (celsius * 9/5) + 32;
 
-// document.write(`O valor em Fahrenheit é: ${fahrenheit}`);
+document.write(`O valor em Fahrenheit é: ${fahrenheit}`);
 
 
 // // 2. Crie um programa que peça um número de 1 a 7 e exiba o dia da
 // // semana correspondente utilizando a estrutura condicional switch.
-// document.write("<br/><br/> Exercicio 2 <br/>")
+document.write("<br/> <br/> <strong>Exercicio 2</strong> <br/>")
 
-// const numeroDia = Number(prompt("Digite um número de 1 a 7 para o dia da semana:"));
+const numeroDia = Number(prompt("Digite um número de 1 a 7 para o dia da semana:"));
 
-// let diaDaSemana;
+let diaDaSemana;
 
-// switch (numeroDia) {
-//   case 1:
-//     diaDaSemana = "Domingo";
-//     break;
-//   case 2:
-//     diaDaSemana = "Segunda-feira";
-//     break;
-//   case 3:
-//     diaDaSemana = "Terça-feira";
-//     break;
-//   case 4:
-//     diaDaSemana = "Quarta-feira";
-//     break;
-//   case 5:
-//     diaDaSemana = "Quinta-feira";
-//     break;
-//   case 6:
-//     diaDaSemana = "Sexta-feira";
-//     break;
-//   case 7:
-//     diaDaSemana = "Sábado";
-//     break;
-//   default:
-//     alert("Numero inválio");
-//     break;
-// }
+switch (numeroDia) {
+  case 1:
+    diaDaSemana = "Domingo";
+    break;
+  case 2:
+    diaDaSemana = "Segunda-feira";
+    break;
+  case 3:
+    diaDaSemana = "Terça-feira";
+    break;
+  case 4:
+    diaDaSemana = "Quarta-feira";
+    break;
+  case 5:
+    diaDaSemana = "Quinta-feira";
+    break;
+  case 6:
+    diaDaSemana = "Sexta-feira";
+    break;
+  case 7:
+    diaDaSemana = "Sábado";
+    break;
+  default:
+    alert("Numero inválio");
+    break;
+}
 
-// document.write(`O dia da semana é: ${diaDaSemana}`)
+document.write(`O dia da semana é: ${diaDaSemana}`)
 
 // // 3. Crie um programa que peça um número e verifique se ele é positivo,
 // // negativo ou zero.
-// document.write("<br/><br/> Exercicio 3 <br/>")
+document.write("<br/> <br/> <strong>Exercicio 3</strong> <br/>")
 
-// const numero = Number(prompt("Digite um número positivo ou negativo:"));
+const numero = Number(prompt("Digite um número positivo ou negativo:"));
 
-// if (numero > 0) {
-//   document.write(`O número ${numero} é positivo.`);
-// } else if (numero < 0) {
-//   document.write(`O número é  ${numero} negativo.`);
-// } else {
-//     document.write("O número é zero.");
-// }
+if (numero > 0) {
+  document.write(`O número ${numero} é positivo.`);
+} else if (numero < 0) {
+  document.write(`O número é  ${numero} negativo.`);
+} else {
+    document.write("O número é zero.");
+}
 
 // // 4. O custo de um carro novo ao consumidor é a soma do custo de
 // // fábrica com a porcentagem do distribuidor e dos impostos
@@ -65,16 +65,16 @@
 // // distribuidor seja de 28% e os impostos de 45%, escrever um
 // // algoritmo para ler o custo de fábrica de um carro,calcular e escrever
 // // o custo final ao consumidor.
-// document.write("<br/><br/> Exercicio 4 <br/>")
+document.write("<br/> <br/> <strong>Exercicio 4</strong> <br/>")
 
-// const custoDaFabrica = Number(prompt("Digite o custo de fábrica do carro:"));
+const custoDaFabrica = Number(prompt("Digite o custo de fábrica do carro:"));
 
-// const valorDistribuidor = 0.28;
-// const valorImpostos = 0.45;
+const valorDistribuidor = 0.28;
+const valorImpostos = 0.45;
 
-// const valorConsumidor = custoDaFabrica + (custoDaFabrica * valorDistribuidor) + (custoDaFabrica * valorImpostos);
+const valorConsumidor = custoDaFabrica + (custoDaFabrica * valorDistribuidor) + (custoDaFabrica * valorImpostos);
 
-// document.write(`O custo final do veículo será de: ${valorConsumidor} reais.`);
+document.write(`O custo final do veículo será de: ${valorConsumidor} reais.`);
 
 
 // // 5. Desenvolva um algoritmo que faça o cálculo do índice de massa
@@ -88,56 +88,56 @@
 // // c. Se o IMC for igual ou maior que 18,5 e igual ou menor que
 // // 24,99, o status a ser mostrado será “Você está dentro da faixa
 // // de peso ideal”.
-// document.write("<br/><br/> Exercicio 5 <br/>")
+document.write("<br/> <br/> <strong>Exercicio 5</strong> <br/>")
 
-// const peso = Number(prompt("Digite seu peso:"));
-// const altura = Number(prompt("Digite sua altura:"));
+const peso = Number(prompt("Digite seu peso:"));
+const altura = Number(prompt("Digite sua altura:"));
 
-// const calculoImc = peso / (altura * altura);
+const calculoImc = peso / (altura * altura);
 
-// if (calculoImc < 18.5) {
-//     document.write(`Você está abaixo da faixa de peso ideal! Seu IMC é de: ${calculoImc.toFixed(2)}`)
-// } else if (calculoImc > 24.99){
-//     document.write(`Você está acima da faixa de peso ideal! Seu IMC é de: ${calculoImc.toFixed(2)}`)
-// } else {
-//     document.write(`Você está dentro da faixa de peso ideal! Seu IMC é de: ${calculoImc.toFixed(2)}`)
-// };
+if (calculoImc < 18.5) {
+    document.write(`Você está abaixo da faixa de peso ideal! Seu IMC é de: ${calculoImc.toFixed(2)}`)
+} else if (calculoImc > 24.99){
+    document.write(`Você está acima da faixa de peso ideal! Seu IMC é de: ${calculoImc.toFixed(2)}`)
+} else {
+    document.write(`Você está dentro da faixa de peso ideal! Seu IMC é de: ${calculoImc.toFixed(2)}`)
+};
 
 
 // // 6. Escreva um algoritmo que solicite 2 números e uma operação
 // // matemática. O algoritmo deve realizar o cálculo com os 2 números
 // // digitados conforme a operação informada.
-// document.write("<br/><br/> Exercicio 6 <br/>")
+document.write("<br/> <br/> <strong>Exercicio 6</strong> <br/>")
 
-// const numero1 = Number(prompt("Digite o primeiro número:"));
-// const operacao = prompt("Escolha a operação desejada (+, -, *, /):");
-// const numero2 = Number(prompt("Digite o segundo número:"));
+const numero1 = Number(prompt("Digite o primeiro número:"));
+const operacao = prompt("Escolha a operação desejada (+, -, *, /):");
+const numero2 = Number(prompt("Digite o segundo número:"));
 
-// let resultado;
+let resultado;
 
-// switch (operacao) {
-//   case "+":
-//     resultado = numero1 + numero2;
-//     break;
-//   case "-":
-//     resultado = numero1 - numero2;
-//     break;
-//   case "*":
-//     resultado = numero1 * numero2;
-//     break;
-//   case "/":
-//     if (numero2 !== 0) {
-//       resultado = numero1 / numero2;
-//     } else {
-//       resultado = "Erro: Divisão por zero.";
-//     }
-//     break;
-//   default:
-//     resultado = "Operação inválida.";
-//     break;
-// }
+switch (operacao) {
+  case "+":
+    resultado = numero1 + numero2;
+    break;
+  case "-":
+    resultado = numero1 - numero2;
+    break;
+  case "*":
+    resultado = numero1 * numero2;
+    break;
+  case "/":
+    if (numero2 !== 0) {
+      resultado = numero1 / numero2;
+    } else {
+      resultado = "Erro: Divisão por zero.";
+    }
+    break;
+  default:
+    resultado = "Operação inválida.";
+    break;
+}
 
-// document.write(`Resultado: ${resultado}`);
+document.write(`Resultado: ${resultado}`);
  
 
 
@@ -155,19 +155,87 @@
 // // ser considerado e também contabilizado o valor da taxa a ser
 // // cobrada pelo saque. Ou seja, o valor a ser sacado somado ao valor
 // // da taxa do saque não pode ser maior que o saldo disponível.
-document.write("<br/><br/> Exercicio 7 <br/>");
+document.write("<br/> <br/> <strong>Exercicio 7</strong> <br/>")
 
-const saldoConta = 2000;
-const valorSaque = Number(prompt("Digite o valor que deseja sacar"));
-const valorTaxa = 4.50;
+let valorSaldo = 2000;
 
-if (valorSaque % 5 === 0 && valorSaque <= saldoConta) {
-    if (valorSaque + valorTaxa <= saldoConta) {
-        saldoConta -= valorTaxa + valorSaque;
-        document.write("Saque realizado com sucesso. Saldo atual: R$" + saldoConta.toFixed(2))
-    } else {
-        document.write("Saldo insuficiente para realizar o saque.")
-    }
+const valorSaque = Number(prompt("Digite o valor que deseja sacar que seja múltiplo de 5:"));
+
+if (valorSaque > 0 && valorSaque % 5 === 0) {
+  const taxaSaque = 4.5;
+  const valorTotalSaque = valorSaque + taxaSaque;
+
+  if (valorTotalSaque <= valorSaldo) {
+    valorSaldo -= valorTotalSaque;
+    document.write(`<br/> Saque de R$ ${valorSaque.toFixed(2)} efetuado com sucesso.`);
+    document.write(`<br/><br/> Taxa de saque de R$ ${taxaSaque.toFixed(2)}`);
+    document.write(`<br/><br/> Saldo disponível: R$ ${valorSaldo.toFixed(2)}`);
+  } else {
+    document.write("<br/><br/> Você não possui saldo suficiente ");
+  }
 } else {
-    document.write("Valor inválido!")
+  alert("Valor de saque inválido. Certifique-se de que seja um número múltiplo de 5.");
+}
+
+
+// 8. Faça um algoritmo que leia a idade e peso de um atleta e imprima
+// a sua categoria, de acordo com a seguinte tabela:
+document.write("<br/> <br/> <strong>Exercicio 8</strong> <br/>")
+
+const idade = Number(prompt("Qual sua idade?"));
+const peso2 = Number(prompt("Quanto você pesa?"));
+
+let categoria;
+let categoriaPeso;
+
+if (idade <= 12) {
+  categoria = "Infantil";
+  categoriaPeso = peso2;
+} else if (idade > 12 && idade <= 16) {
+  categoria = "juvenil";
+
+  if (peso2 <= 40) {
+    categoriaPeso = peso2;
+    categoria = "<br>Juvenil leve";
+  } else {
+    categoriaPeso = peso2;
+    categoria = "Juvenil Pesado";
+  }
+} else if (idade >= 17 && idade <= 24) {
+  categoria = "senior";
+  if (peso2 <= 45) {
+    categoriaPeso = peso2;
+    categoria = "Senior leve";
+  } else if (peso2 >= 45 && peso2 <= 60) {
+    categoriaPeso = peso2;
+    categoria = "Senior Médio";
+  } else {
+    categoriaPeso = peso2;
+    categoria = "Senior Pesado";
+  }
+} else {
+  categoria = "Veterano";
+  categoriaPeso = peso2;
+}
+document.write(`<br/><br/> Seu peso é ${categoriaPeso}Kg`);
+document.write(`<br/><br/> Sua categoria é ${categoria}`);
+
+
+// 9. Escreva um programa que pergunte a velocidade de um carro. Caso
+// ultrapasse 80Km/h, exiba uma mensagem dizendo que o usuário foi
+// multado. Nesse caso, exiba o valor da multa, cobrando R$5 por cada
+// km acima da velocidade permitida.
+document.write("<br/> <br/> <strong>Exercicio 9</strong> <br/>")
+
+const velocidadeCarro = Number(prompt("Digite a velocidade do carro em Km/h:"));
+
+if (velocidadeCarro > 80) {
+  const velocidadeExcedida = velocidadeCarro - 80;
+  const valorMulta = velocidadeExcedida * 5;
+
+  document.write("<br/><br/> Você foi multado por excesso de velocidade!");
+  document.write(`<br/><br/> Velocidade excedida: ${velocidadeExcedida} Km/h`);
+  document.write(`<br/><br/> Valor da multa: R$${valorMulta.toFixed(2)}`);
+} else {
+  document.write("<br/><br/> Você está dentro do limite de velocidade permitido.");
 }
